6401b4dadd2e86e58797ab0ed22586a303ebea4c
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-02 18:05:44 +0530

    Selection-sorting algorithm

---
+++ Sorting-Algorithms/SelectionSort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void  selectionSort(int arr[], int size){
+  for (int i=0;i<size-2;i++){
+    int smallest = i;
+    for(int j=i+1; j<=size-1;j++){
+      if(arr[j]<arr[smallest]){
+        smallest = j;
+      }
+    }
+    swap(arr[i],arr[smallest]);
+  }
+};
+
+int main(){
+  int size;
+  cin>>size;
+  int arr[size];
+  for(int i=0; i<size;i++){
+    cin >> arr[i];
+  }
+  selectionSort(arr, size);
+  for(int i=0; i<size;i++){
+    cout << arr[i] << " ";
+  }
+  return 0;
+};

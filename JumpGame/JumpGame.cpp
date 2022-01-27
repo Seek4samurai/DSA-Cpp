6ecc4e88eb98c70d47217dfebc60bb989072f41d
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-01-27 19:41:56 +0530

    Jump Game algorithm

---
+++ JumpGame/JumpGame.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  bool canJump(vector<int>& nums) {
+    int size = nums.size();
+    int goodIndex = size-1;
+
+    for(int i=size-2; i>=0; i--){
+      if(i+nums[i] >= goodIndex){
+        goodIndex = i;
+      }
+    }
+
+    if(goodIndex == 0) return true;
+    else return false;
+  }
+};
+
+int main(){
+  vector<int> vector1 = {2,3,1,1,4};
+
+  Solution Obj;
+  cout << Obj.canJump(vector1);
+  return 0;
+};

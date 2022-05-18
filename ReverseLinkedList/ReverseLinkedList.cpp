4f2daa89f1169105e57e4cdfd8a4ee627f386552
Gourav Singh Rawat <grvrawat999@gmail.com>
2022-05-18 23:16:58 +0530

    Reversed Linked list

---
+++ ReverseLinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+  ListNode *reverseList(ListNode *head) {
+    ListNode *current = head;
+    ListNode *previous = NULL;
+
+    while (current != NULL) {
+      ListNode *n = current->next;
+      current->next = previous;
+
+      previous = current;
+      current = n;
+    }
+    return previous;
+  }
+};
+
+int main() { return 0; };

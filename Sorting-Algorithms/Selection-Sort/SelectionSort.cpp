b21428d38e0fe35579f64ab226529dd20fce1b80
Seek4samurai <grvrawat999@gmail.com>
2022-07-15 20:57:31 +0530

    Selection sort

--- Sorting-Algorithms/Selection-Sort/SelectionSort.cpp
+++ Sorting-Algorithms/Selection-Sort/SelectionSort.cpp
@@ -1,27 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void  selectionSort(int arr[], int size){
-  for (int i=0;i<size-2;i++){
+void selectionSort(int arr[], int size) {
+  for (int i = 0; i <= size - 2; i++) {
     int smallest = i;
-    for(int j=i+1; j<=size-1;j++){
-      if(arr[j]<arr[smallest]){
+    for (int j = i + 1; j <= size - 1; j++) {
+      if (arr[j] < arr[smallest]) {
         smallest = j;
       }
+      swap(arr[i], arr[smallest]);
     }
-    swap(arr[i],arr[smallest]);
   }
 };
 
-int main(){
-  int size;
-  cin>>size;
-  int arr[size];
-  for(int i=0; i<size;i++){
-    cin >> arr[i];
-  }
+int main() {
+  int size = 5;
+  int arr[] = {4, 2, 5, 3, 1};
+
   selectionSort(arr, size);
-  for(int i=0; i<size;i++){
+
+  for (int i = 0; i < size; i++) {
     cout << arr[i] << " ";
   }
   return 0;
